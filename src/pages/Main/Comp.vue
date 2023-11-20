794654637d0246984ebecf6da73ed327466a199f
AndyWu <andy_wu@softmobile.com.tw>
2023-11-20 23:00:04 +0800

    feat icon and fix comp animation css

--- src/pages/Main/Comp.vue
+++ src/pages/Main/Comp.vue
@@ -1,25 +1,43 @@
 <template>
-  <div  class="comp-main">
-    <div class="comp-center row" v-scroll-fire="handleScroll">
-      <div  class="col-3">
-        <h5 class="comp-font">
+  <div class="comp-main">
+    <div class="comp-center row" v-scroll="handleScroll">
+      <div class="col-3 comp-font animated" ref="compFont">
+        <h5>
           <p>2人1板</p>
           <p>即可開打</p>
-          <div class="comp-img-bat"><img src="../../assets/icon03.png"/></div>
+          <div class="comp-img-bat">
+            <img
+              src="../../assets/icon03.png"
+              class="animated1"
+              ref="compImgBat"
+            />
+          </div>
         </h5>
       </div>
-      <div  class="col-3">
-        <h5 class="comp-font">
+      <div class="col-3 comp-font animated" ref="compFont2">
+        <h5>
           <p>快速上手</p>
           <p>多種變化球</p>
-          <div class="comp-img-ball"><img src="../../assets/icon02.png" /></div>
+          <div class="comp-img-ball">
+            <img
+              src="../../assets/icon02.png"
+              class="animated1"
+              ref="compImgBall"
+            />
+          </div>
         </h5>
       </div>
-      <div  class="col-3">
-        <h5 class="comp-font">
+      <div class="col-3 comp-font animated" ref="compFont3">
+        <h5>
           <p>安全方便</p>
           <p>場地不受限</p>
-          <div class="comp-img-land"><img src="../../assets/icon01.png" /></div>
+          <div class="comp-img-land">
+            <img
+              src="../../assets/icon01.png"
+              class="animated1"
+              ref="compImgLand"
+            />
+          </div>
         </h5>
       </div>
     </div>
@@ -27,11 +45,42 @@
 </template>
 
 <script setup>
-import { event } from "quasar";
-import { ref } from "vue";
-const handleScroll = (event) => {
-  event.classList.add("comp-main-animation"); // 假設你有一個 CSS 動畫類
+import { ref, onMounted, onBeforeUnmount } from "vue";
+
+const compFont = ref(null);
+const compFont2 = ref(null);
+const compFont3 = ref(null);
+const compImgBat = ref(null);
+const compImgBall = ref(null);
+const compImgLand = ref(null);
+
+const handleScroll = () => {
+  const triggerPosition = window.innerHeight / 2; // You can adjust this position as needed
+
+  checkAndAnimate(compFont, "fadeInRight", triggerPosition);
+  checkAndAnimate(compFont2, "fadeInRight", triggerPosition);
+  checkAndAnimate(compFont3, "fadeInRight", triggerPosition);
+  checkAndAnimate(compImgBat, "fadeInRight", triggerPosition);
+  checkAndAnimate(compImgBall, "fadeInRight", triggerPosition);
+  checkAndAnimate(compImgLand, "fadeInRight", triggerPosition);
 };
+
+const checkAndAnimate = (elementRef, animationClass, triggerPosition) => {
+  const element = elementRef.value;
+  const elementTop = element.getBoundingClientRect().top;
+
+  if (elementTop < triggerPosition) {
+    element.classList.add(animationClass);
+  }
+};
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
 </script>
 
 <style></style>
--- src/pages/Main/News.vue
+++ src/pages/Main/News.vue
@@ -17,7 +17,7 @@
             <p class="news-detail-content">
               CTWB最新球衣已經到貨！採用熱昇華印刷與排汗的面料，能夠有效地排出汗水，讓你在比賽或訓練中保持乾爽舒適，使它們既舒適又時尚。還在等什麼？不要錯過這次機會，立即選購CTWB的最新球衣吧！
             </p>
-            <q-btn class="btn-font-size" color="orange" label="了解更多" size="20px" rounded dense />
+            <q-btn class="btn-font-size" color="orange" label="了解更多" size="20px" rounded dense @click="clickMany()"/>
           </div>
         </div>
       </div>
@@ -30,7 +30,7 @@
               威浮球，本名Wiffle
               Ball，為一種改良過的安全類棒球。可以3v3到6v6皆可，類似棒球的規則，但卻沒有正式棒球複雜，且隨處可打，不怕打傷人或打壞東西，是一項適合老少一童同樂的運動，現在就來認識看看吧!。
             </p>
-            <q-btn class="btn-font-size" color="orange" label="了解更多" size="20px" rounded dense />
+            <q-btn class="btn-font-size" color="orange" label="了解更多" size="20px" rounded dense @click="clickMany()"/>
 
           </div>
         </div>
@@ -49,7 +49,7 @@
             <p class="news-detail-content">
               CTWB致力於辦各項威浮球比賽與體驗活動，只要你對威浮球有興趣，十分歡迎超級萌新來體驗這項不會受傷又能享受球速的運動。也歡迎各路好手與我們接洽，來進行威浮球友誼賽。
             </p>
-            <q-btn class="btn-font-size" color="orange" label="了解更多" size="20px" rounded dense />
+            <q-btn class="btn-font-size" color="orange" label="了解更多" size="20px" rounded dense @click="clickMany()"/>
 
           </div>
         </div>
@@ -58,8 +58,12 @@
   </main>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { useRouter } from "vue-router";
+const router = useRouter();
+const clickMany = ()=>{
+  router.push("/" + 'about');
+}
 </script>
 
 <style></style>
