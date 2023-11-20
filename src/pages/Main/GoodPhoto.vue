accc3ffb06f5011d9d03e87972dffa4494507ddb
AndyWu <andy_wu@softmobile.com.tw>
2023-11-20 17:12:23 +0800

    Add swiper photo in GoodPhoto page

--- src/pages/Main/GoodPhoto.vue
+++ src/pages/Main/GoodPhoto.vue
@@ -10,49 +10,77 @@
         />
         <!-- <img src="../../assets/goodPhoto/photo (1).JPG" -->
         <p class="good-photo-content">歡迎新舊玩家一同參與</p>
-        <div id="q-app">
-      <q-carousel
-        swipeable
-        animated
-        arrows
-        dark
-        class="good-photo-carousel"
-        control-type="regular"
-        :autoplay="autoplay"
-        v-model="slide"
-        infinite
-        transition-prev="slide-right"
-        transition-next="slide-left"
-        @mouseenter="autoplay = false"
-        @mouseleave="autoplay = true"
-      >
-        <q-carousel-slide
-          v-for="(photo, index) in photoArray"
-          :name="index"
-          :key="index"
-          :img-src="photo.url"
-        />
-        <!-- <q-carousel-slide
-              :name="2"
-              img-src="../../assets/information2.png"
+        <div class="swiper-photo">
+          <swiper
+            :loop="false"
+            :slides-per-view="4"
+            :navigation="true"
+            :modules="modules"
+            class="mySwiper3"
+          >
+            <swiper-slide v-for="(photo, index) in photoArray" :key="index">
+              <img :src="photo.url" @click="openModal(index)" />
+            </swiper-slide>
+          </swiper>
+        </div>
+        <q-btn class="btn-font-size" color="orange" label="更多精彩相片" size="20px" rounded dense @click="clickManyPhoto()"/>
+        <!-- Image Modal -->
+        <q-dialog v-model="icon" full-width full-height>
+          <div style="background-color: white">
+            <q-btn
+              icon="close"
+              flat
+              round
+              dense
+              v-close-popup
+              @click="closeModal"
+              style="float: right"
             />
-            <q-carousel-slide
-              :name="3"
-              img-src="../../assets/information3.png"
-            /> -->
-      </q-carousel>
-    </div>
+            <swiper
+              :initialSlide="currentImageIndex"
+              :modules="modules"
+              :navigation="true"
+              class="mySwiper4"
+            >
+              <swiper-slide v-for="(photo, index) in photoArray" :key="index">
+                <div style="display: flex; align-items: center">
+                  <img :src="photo.url" width="1200" height="800" />
+                </div>
+              </swiper-slide>
+            </swiper>
+          </div>
+        </q-dialog>
       </div>
     </div>
-
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
-const slide = ref(1);
-const autoplay = ref(3000);
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+import { useRouter } from "vue-router";
+import { FreeMode, Navigation } from "swiper/modules";
+
+
+const router = useRouter();
+const icon = ref(false);
+let currentImageIndex = ref(0);
+
+const closeModal = () => {
+  icon.value = false;
+};
+const clickManyPhoto = ()=>{
+  router.push("/" + 'photo');
+}
 const photoArray = ref([
+  { url: "src/assets/goodPhoto/IMG_9500.JPG" },
+  { url: "src/assets/goodPhoto/IMG_9486.JPG" },
+  { url: "src/assets/goodPhoto/IMG_9406.JPG" },
+  { url: "src/assets/goodPhoto/IMG_9286.JPG" },
   { url: "src/assets/goodPhoto/photo1.JPG" },
   { url: "src/assets/goodPhoto/photo2.JPG" },
   { url: "src/assets/goodPhoto/photo3.JPG" },
@@ -70,6 +98,12 @@ const photoArray = ref([
   { url: "src/assets/goodPhoto/photo15.JPG" },
   { url: "src/assets/goodPhoto/photo16.JPG" },
 ]);
+const modules = [FreeMode, Navigation];
+const openModal = (index) => {
+  console.log(index);
+  icon.value = !icon.value;
+  currentImageIndex = index;
+};
 </script>
 
 <style></style>
