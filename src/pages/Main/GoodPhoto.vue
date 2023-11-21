fa52e9611dba6be48618208b2c1cb8c5bc4c9ae4
AndyWu <andy_wu@softmobile.com.tw>
2023-11-21 14:36:44 +0800

    feat getImgUrl function

--- src/pages/Main/GoodPhoto.vue
+++ src/pages/Main/GoodPhoto.vue
@@ -52,7 +52,7 @@
             >
               <swiper-slide v-for="(photo, index) in photoArray" :key="index">
                 <div style="display: flex; align-items: center">
-                  <img :src="photo.url" width="1200" height="800" />
+                  <img :src="getImgUrl(photo.url)" width="1200" height="800" />
                 </div>
               </swiper-slide>
             </swiper>
@@ -83,27 +83,30 @@ const closeModal = () => {
 const clickManyPhoto = () => {
   router.push("/" + "photo");
 };
+const getImgUrl = (url) =>{
+  return new URL(url, import.meta.url).href;
+}
 const photoArray = ref([
-  { url: "/assets/IMG_9500.png" },
-  { url: "/assets/IMG_9486.png" },
-  { url: "/assets/IMG_9406.png" },
-  { url: "/assets/IMG_9286.png" },
-  { url: "/assets/photo1.png" },
-  { url: "/assets/photo2.png" },
-  { url: "/assets/photo3.png" },
-  { url: "/assets/photo4.png" },
-  { url: "/assets//photo5.png" },
-  { url: "/assets/photo6.png" },
-  { url: "/assets/photo7.png" },
-  { url: "/assets/photo8.png" },
-  { url: "/assets/photo9.png" },
-  { url: "/assets/photo10.png" },
-  { url: "/assets/photo11.png" },
-  { url: "/assets/photo12.png" },
-  { url: "/assets/photo13.png" },
-  { url: "/assets/photo14.png" },
-  { url: "/assets/photo15.png" },
-  { url: "/assets/photo16.png" },
+  { url: "/src/assets/IMG_9500.png" },
+  { url: "/src/assets/IMG_9486.png" },
+  { url: "/src/assets/IMG_9406.png" },
+  { url: "/src/assets/IMG_9286.png" },
+  { url: "/src/assets/photo1.png" },
+  { url: "/src/assets/photo2.png" },
+  { url: "/src/assets/photo3.png" },
+  { url: "/src/assets/photo4.png" },
+  { url: "/src/assets//photo5.png" },
+  { url: "/src/assets/photo6.png" },
+  { url: "/src/assets/photo7.png" },
+  { url: "/src/assets/photo8.png" },
+  { url: "/src/assets/photo9.png" },
+  { url: "/src/assets/photo10.png" },
+  { url: "/src/assets/photo11.png" },
+  { url: "/src/assets/photo12.png" },
+  { url: "/src/assets/photo13.png" },
+  { url: "/src/assets/photo14.png" },
+  { url: "/src/assets/photo15.png" },
+  { url: "/src/assets/photo16.png" },
 ]);
 const modules = [FreeMode, Navigation];
 const openModal = (index) => {
